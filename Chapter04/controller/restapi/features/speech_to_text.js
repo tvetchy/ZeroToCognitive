6668366880f918e9a0eea9c2d2a1e7c310780e38
tvetchy <thomas_vetchy@at.ibm.commarix17>
2017-03-17 10:38:30 +0100

    Addopted during Lesson

--- Chapter04/controller/restapi/features/speech_to_text.js
+++ Chapter04/controller/restapi/features/speech_to_text.js
@@ -13,29 +13,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var extend = require('extend');
-var watson = require('watson-developer-cloud');
-var vcapServices = require('vcap_services');
-var config = require('../../env.json');
+ var extend = require('extend');
+ var watson = require('watson-developer-cloud');
+ var vcapServices = require('vcap_services');
 
-exports.stt_token = function(req, res) {
-    var sttConfig = extend(config.speech_to_text, vcapServices.getCredentials('speech_to_text'));
+ var config = require('../../env.json');
 
-    var sttAuthService = watson.authorization(sttConfig);
+ exports.stt_token = function(req, res) {
+     var sttConfig = extend(config.speech_to_text, vcapServices.getCredentials('speech_to_text'));
 
-    sttAuthService.getToken({
-        url: sttConfig.url
-    }, function(err, token) {
-        if (err) {
-            console.log('Error retrieving speech to text token: ', err);
-            res.status(500).send('Error retrieving speech to text token');
-            return;
-        }
-        res.send(token);
-    });
-}
+     var sttAuthService = watson.authorization(sttConfig);
 
-exports.tts_synthesize = function(req, res) {
-  console.log("tts_synthesize entered");
+     sttAuthService.getToken({
+         url: sttConfig.url
+     }, function(err, token) {
+         if (err) {
+             console.log('Error retrieving token: ', err);
+             res.status(500).send('Error retrieving token');
+             return;
+         }
+         res.send(token);
+     });
+ }
 
-}
+ exports.tts_synthesize = function(req, res) {
+   console.log("tts_synthesize entered");
+     var ttsConfig = watson.text_to_speech(config.text_to_speech);
+     var transcript = ttsConfig.synthesize(req.query);
+     transcript.on('response', function(response) {
+       if (req.query.download) {
+         response.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
+       }
+     });
+     transcript.on('error', function(error) { console.log("error encountered: "+error); error; });
+     transcript.pipe(res);
+ }
